4c449d0fec1907e647d50280f81fa55a08c18bad
ovitest123 <oparsiunas2@googlemail.com>
2020-08-06 02:45:54 +0000

    another one

--- src/app/tools/imageList/imageList.js
+++ src/app/tools/imageList/imageList.js
@@ -296,7 +296,7 @@ function addSingleImageToList(imageMetadata, imageData) {
 function addImageFromMultiUploadToList(imageMetadata, imageData, isfirstFromMany) {
   addNewImage(imageMetadata.name, imageData);
   setDefaultImageProperties(images[newImageId], imageMetadata);
-  if (images.length === 1 || isfirstFromMany || images.length === 3) {
+  if (images.length === 1 || isfirstFromMany || images.length === 4) {
     highlightImageThumbnail(images[newImageId].thumbnailElementRef.childNodes[1]);
     saveAndRemoveCurrentImageDetails();
     changeCurrentImageNameElementText(imageMetadata.name, isfirstFromMany);
