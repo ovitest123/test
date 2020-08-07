108cff1f7c63e46a54fa14f07451feed4e2d4c87
ovitest123 <oparsiunas2@googlemail.com>
2020-08-07 21:59:17 +0000

    to the original directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,61 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const FailOnErrorsPlugin = require('fail-on-errors-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CNAMEWebpackPlugin = require('cname-webpack-plugin');
 
 const env = process.env.NODE_ENV || 'development';
 
 module.exports = () => {
-  const outputFileName = env === 'production' ? '[name].[contenthash].js' : '[name].js';
-  const externalsFileExtension = env === 'production' ? '.min.js' : '.js';
-  const plugins = [
+  let plugins = [];
+  let config = {};
+  if (env === 'production') {
+    config = {
+      loggingLevel: 'normal',
+      outputFileName: 'public/[name].[contenthash].js',
+      externalsFileExtension: '.min.js',
+      externalsDirectory: 'externalsProd',
+    };
+    plugins = plugins.concat([
+      new CleanWebpackPlugin(),
+      new CNAMEWebpackPlugin({
+        domain: 'myvision.ai',
+      })]);
+  } else {
+    config = {
+      loggingLevel: { assets: false, modules: false, children: false },
+      outputFileName: 'public/[name].js',
+      externalsFileExtension: '.js',
+      externalsDirectory: 'externalsDev',
+    };
+  }
+  plugins = plugins.concat([
     new FailOnErrorsPlugin({
       failOnErrors: true,
       failOnWarnings: true,
     }),
     new HtmlWebpackPlugin({
-      externalsFileExtension,
+      externalsFileExtension: config.externalsFileExtension,
       template: 'src/devIndexTemplate.html',
       minify: false,
-    })];
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/assets/css', to: 'public/assets/css' },
+        { from: './src/assets/images', to: 'public/assets/images' },
+        { from: './src/assets/svg', to: 'public/assets/svg' },
+        { from: `./src/assets/externals/${config.externalsDirectory}`, to: 'public/assets/externals' },
+      ],
+    }),
+  ]);
   return {
     entry: {
       browserSupportBundle: './src/browserSupport/index.js',
       appBundle: './src/app/index.js',
     },
     output: {
-      filename: outputFileName,
-      path: path.resolve(__dirname, './arbitraryFolder4'),
+      filename: config.outputFileName,
+      path: __dirname,
     },
     externals: {
       fabric: 'fabric',
@@ -49,6 +80,7 @@ module.exports = () => {
       ],
     },
     mode: env,
+    stats: config.loggingLevel,
     plugins,
     performance: {
       maxEntrypointSize: 340000,
